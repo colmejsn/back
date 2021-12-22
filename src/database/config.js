b290918bccacc19aa2b363b6306968c1ac308796
colmejson <jeissona.colmenaresc@ecci.edu.co>
2021-12-22 18:51:29 -0500

    dev

--- src/database/config.js
+++ src/database/config.js
@@ -6,7 +6,7 @@ export const dbConnection = async () => {
 
     const { MONGO_DB_PROD, MONGO_DB_TEST, NODE_ENV } = process.env;
 
-    const connectionString = NODE_ENV === "test" ? MONGO_DB_TEST : MONGO_DB_PROD;
+    const connectionString = NODE_ENV === "dev" ? MONGO_DB_TEST : MONGO_DB_PROD;
 
     console.log("esta es la conexión", connectionString);
 
